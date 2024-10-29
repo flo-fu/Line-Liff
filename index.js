9986a6f16f66499cff0ccd4331f3f0a7421464e7
flo-fu <flo.fu@oeya.com>
2024-10-29 17:21:47 +0800

    增加error log

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
   liff
     .init({ liffId: process.env.LIFF_ID })
     .then((res) => {
-        console.log("Success! you can do something with LIFF API here.")
+        console.log("Success! you can do something with LIFF API here." + "LIFF_ID = "  + process.env.LIFF_ID)
         document.getElementById('log').innerText = JSON.stringify(res, null, 4);
     })
     .catch((error) => {
