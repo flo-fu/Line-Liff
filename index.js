b277bf7d55d680afdd9ed5db735b5792bfe049d3
flo-fu <flo.fu@oeya.com>
2024-10-29 18:05:17 +0800

    增加log profile

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ import './index.css';
 import liff from '@line/liff'
 
 document.addEventListener("DOMContentLoaded", function() {
-  liff
-    .init({
+  liff.init({
         liffId: process.env.LIFF_ID
     })
     .then(() => {
         console.log("Success! you can do something with LIFF API here." + "LIFF_ID = "  + process.env.LIFF_ID)
         liff.getProfile((profile) => {
+            document.getElementById('userId').innerHTML = profile.userId;
+            document.getElementById('displayName').innerHTML = profile.displayName;
+            document.getElementById('pictureUrl').innerHTML = profile.pictureUrl;
+            document.getElementById('statusMessage').innerHTML = profile.statusMessage;
             document.getElementById('log').innerText = JSON.stringify(profile, null, 4);
         });
     })
